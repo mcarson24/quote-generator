bf917dfe6cc7b8b9401cdcc58149eec889f2a451
Matthew Carson <matthew24carson@gmail.com>
2016-12-18 22:38:03 -0500

    Start styling applicaiton

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,5 +1,17 @@
 <template>
-    
+    <div id="card" v-bind:style="{ 'background-color': color }" v-show="quote">
+        <div id="quote">
+            <p class="quote">"{{ quote }}"</p>
+            <p class="author">- {{ author }}</p>
+        </div>
+        <div class="buttons">
+            <button @click="getAQuote" class="fetch" v-bind:style="{ color: color }">{{ buttonText }}</button>
+            <div>
+                <i class="fa fa-twitter-square twitter" aria-hidden="true"></i>
+                
+            </div>
+        </div>  
+    </div>
 </template>
 
 <script>
@@ -7,18 +19,100 @@
         data() {
             return {
                 quote: '',
-                author: ''
+                author: '',
+                color: '',
+                buttonText: 'Get A Quote'
             }
         },
         methods: {
             getAQuote() {
-                this.quote = 'quote'
-                this.author = 'author'
+                let api = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+                setTimeout(() => {
+                    this.$http.get(api).then(response => {
+                        this.buttonText = 'Fetching...';
+                        let quoteInfo = response.body;
+
+                        if (quoteInfo == null) {
+                            this.getAQuote();
+                        }
+
+                        this.quote = quoteInfo.quoteText.trim();
+                        this.author = quoteInfo.quoteAuthor;
+
+                        if (quoteInfo.quoteText == null) {
+                            this.getAQuote();
+                        }
+
+                        if (quoteInfo.quoteAuthor === '') {
+                            this.author = 'Unknown';
+                        }
+                        this.color = this.randomColor();
+                        this.buttonText = 'Get A Quote';
+                    }, response => {
+                        this.getAQuote();
+                    });
+                }, 1000);
+            },
+            randomColor() {
+                let characters = "1 2 3 4 5 6 7 8 9 0 A B C D E".split(' ');
+                let color = ['#'];
+                for (let i = 0; i < 6; i++) {
+                    let randomNumber = Math.floor((Math.random() * 15))
+                    color.push(characters[randomNumber]);
+                }
+                return color.join('');
             }
+        },
+        created() {
+            this.getAQuote();
+            this.color = this.randomColor();
         }
     }
 </script>
 
-<style>
-    
+<style lang="scss">
+    #card {
+        width: 50%;
+        height: 150px;
+        margin: 0 auto;
+        border-radius: 5px;
+        padding: 35px;
+        font-weight: bold;
+        display: table;
+        p {
+            color: white;
+            font-size: 1.2em;
+            &.author {
+                text-align: right;
+            }
+            &.quote {
+                text-align: left;
+            }
+        }
+    }
+
+    .buttons {
+        display: block;
+    }
+
+    button {
+        &.fetch {
+            background-color: #FFF;
+            display: inline-block;
+            font-size: 1.1em;
+            font-weight: bold;  
+            border-radius: 5px;
+            float: right;
+            padding: 15px;
+            cursor: pointer;
+        }
+        border: none;
+    }
+    i {
+        &.twitter {
+            float: left;
+            font-size: 60px;
+            color: white;
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+window.Vue = Vue
+require('vue-resource')
 import App from './App'
 
 /* eslint-disable no-new */
