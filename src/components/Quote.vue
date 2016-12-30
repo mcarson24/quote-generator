18be7c618b812d5d5e7327ec99b9d273e16a5205
Matthew Carson <matthew24carson@gmail.com>
2016-12-30 16:22:50 -0500

    Limit quote string to 140 characters when being tweeted

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -67,6 +67,9 @@
         computed: {
             tweetLink() {
                 let encodedQuote = `${this.quote} - ${this.author}` 
+                if (encodedQuote.length >= 33) {
+                    encodedQuote = encodedQuote.substring(0, 130) + '...'
+                }
                 return `https://twitter.com/intent/tweet?text=${encodedQuote.replace(/\s/g, '%20')}&hashtags=quote`
             }
         },
